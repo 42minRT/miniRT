789c4532f68c0dbd0f7ff82a9550d49592e1c50c
sumnii <sumsong@student.42seoul.kr>
2022-10-30 20:17:10 +0900

    [FEAT] FOV to focal_len

--- include/scene.h
+++ include/scene.h
@@ -4,11 +4,14 @@
 # include "structures.h"
 # include "vector_utils.h"
 
-t_canvas    canvas(int  width, int height);
-t_camera    camera(t_canvas *canvas, t_point3 origin);
+# define _USE_MATH_DEFINES
+
+t_canvas	canvas(int width, int height);
+t_camera	camera(t_canvas *canvas, t_point3 origin);
 t_sphere	*new_sphere(t_point3 center, double radius);
 t_object	*new_object(t_object_type type, void *element, t_color3 albedo);
-t_light		*new_light_point(t_point3 light_origin, t_color3 light_color, double bright_ratio);
+t_light		*new_light_point(t_point3 light_origin, t_color3 light_color,
+			double bright_ratio);
 t_scene		*scene_init(void);
 
 #endif
