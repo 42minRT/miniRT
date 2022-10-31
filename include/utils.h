399843cde61a018987c98598c68520e46e5d4487
yeonseong <yeonseong.dev@gmail.com>
2022-10-31 20:27:11 +0900

    FEAT: free_splited 함수 추가

--- include/utils.h
+++ include/utils.h
@@ -4,6 +4,7 @@
 # include "structures.h"
 
 t_is_return	free_return(char **target, t_is_return boolean);
+void		free_spited(char **target);
 double		ft_atod(char *str);
 int			count_elements(char **elements);
 char		**get_elements_by_type(t_rt_list *file, char *type);
--- src/utils/free_return.c
+++ src/utils/free_return.c
@@ -1,5 +1,19 @@
 #include "../../include/structures.h"
 
+void	free_spited(char **target)
+{
+	int	i;
+
+	i = 0;
+	while (target && target[i])
+	{
+		free(target[i]);
+		++i;
+	}
+	if (target)
+		free(target);
+}
+
 t_is_return	free_return(char **target, t_is_return boolean)
 {
 	int	i;
