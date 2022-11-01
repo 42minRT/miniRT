137abbd55bb58ddf9fd8001b6b0b70812c0aa4e4
김재환 <woghks7859@gmail.com>
2022-11-01 17:18:14 +0900

    CHORE: 공백 탭으로 바꿈.

--- src/scene/scene_init.c
+++ src/scene/scene_init.c
@@ -8,16 +8,16 @@ t_scene	*scene_init(t_rt_list *file)
 	t_scene		*scene;
 	t_object	*world;
 	t_object	*lights;
-    double ka;
+	double ka;
 
-    scene = malloc(sizeof(t_scene));
+	scene = malloc(sizeof(t_scene));
 	if (!scene)
 		return (NULL);
-    scene->canvas = canvas(400, 300);
-    scene->ambient = set_ambient(get_elements_by_type(file, "A"));
-    scene->camera = set_camera(&scene->canvas, get_elements_by_type(file, "C"));
-    world = new_object(SP, new_sphere(point3(-2, 0, -5), 2), color3(0.5, 0, 0));
-    object_append(&world, new_object(SP, new_sphere(point3(2, 0, -5), 2), color3(0, 0.5, 0)));
+	scene->canvas = canvas(400, 300);
+	scene->ambient = set_ambient(get_elements_by_type(file, "A"));
+	scene->camera = set_camera(&scene->canvas, get_elements_by_type(file, "C"));
+	world = new_object(SP, new_sphere(point3(-2, 0, -5), 2), color3(0.5, 0, 0));
+	object_append(&world, new_object(SP, new_sphere(point3(2, 0, -5), 2), color3(0, 0.5, 0)));
 	object_append(&world, new_object(SP, new_sphere(point3(0, -1000, 0), 995), color3(1, 1, 1)));
 	scene->world = world;
 	lights = new_object(LIGHT_POINT, new_light_point(point3(0, 25, 0), color3(1, 1, 1), 0.5), color3(0, 0, 0));
