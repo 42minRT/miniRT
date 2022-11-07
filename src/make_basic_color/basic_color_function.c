8bbb86fb2ebd5e473d8979a460dc106051b272e9
김재환 <woghks7859@gmail.com>
2022-11-08 01:55:46 +0900

    REFACT: basic color

--- src/make_basic_color/basic_color_function.c
+++ src/make_basic_color/basic_color_function.c
@@ -9,28 +9,3 @@ t_color3	set_color_rgb(double r, double g, double b)
 	color.z = b;
 	return (color);
 }
-
-t_color3	red(void)
-{
-	return (set_color_rgb(255, 0, 0));
-}
-
-t_color3	green(void)
-{
-	return (set_color_rgb(0, 255, 0));
-}
-
-t_color3	blue(void)
-{
-	return (set_color_rgb(0, 0, 255));
-}
-
-t_color3	white(void)
-{
-	return (set_color_rgb(255, 255, 255));
-}
-
-t_color3	black(void)
-{
-	return (set_color_rgb(0, 0, 0));
-}
