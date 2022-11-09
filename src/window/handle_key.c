34cd81e45adc4a99f0bbdd88a7c7f9498f91bfa5
yeonseong <yeonseong.dev@gmail.com>
2022-11-10 00:53:15 +0900

    REFACT: change function name

--- src/window/handle_key.c
+++ src/window/handle_key.c
@@ -5,14 +5,16 @@
 #include "../../mlx/mlx.h"
 #include "../../include/vector_utils.h"
 
-static void	rotate_camera(t_scene *scene, t_vec3 axis)
+static void	plus_camera_direction(t_scene *scene, t_vec3 axis)
 {
 	t_camera	cam;
 	double		step;
+	t_vec3		temp;
 
 	step = 0.042;
 	cam = scene->camera;
-	cam.w_dir = vunit(vplus(vmult(axis, step), cam.w_dir));
+	temp = vplus(vmult(axis, step), cam.w_dir);
+	cam.w_dir = vunit(temp);
 	set_cam_coordination(&cam.u_dir, &cam.v_dir, &cam.w_dir);
 	cam.horizontal = vmult(cam.u_dir, cam.viewport_w);
 	cam.vertical = vmult(cam.v_dir, cam.viewport_h);
@@ -31,23 +33,19 @@ void	handle_key(int keycode, void *arg)
 		close_window(EXIT_SUCCESS);
 	if (keycode == KEY_Q)
 	{
-		(void) scene;
-		// x 기준으로 ++
-		rotate_camera(scene, vec3(1, 0, 0));
+		plus_camera_direction(scene, vec3(1, 0, 0));
 		draw_image(scene);
 		mlx_put_image_to_window(scene->img.mlx_ptr, scene->img.win_ptr, scene->img.img, 0, 0);
 	}
 	if (keycode == KEY_W)
 	{
-		// y기준으로 ++ 
-		rotate_camera(scene, vec3(0, 1, 0));
+		plus_camera_direction(scene, vec3(0, 1, 0));
 		draw_image(scene);
 		mlx_put_image_to_window(scene->img.mlx_ptr, scene->img.win_ptr, scene->img.img, 0, 0);
 	}
 	if (keycode == KEY_E)
 	{
-		// z축 기준으로 ++ 
-		rotate_camera(scene, vec3(0, 0, 1));
+		plus_camera_direction(scene, vec3(0, 0, 1));
 		draw_image(scene);
 		mlx_put_image_to_window(scene->img.mlx_ptr, scene->img.win_ptr, scene->img.img, 0, 0);
 	}
